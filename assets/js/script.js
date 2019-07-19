e43e703927220b659a73ac1b70cb3ebbac2ecb7c
aaron <aaronbushman@gmail.com>
2019-07-18 20:36:58 -0400

    latest update

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+$(function(){
+    $("#aboutme").hide();
+    $("#portfolio").hide();
+    $("#contactme").hide();
+    $("#firstdiv").show();
+
+    $("#head3").click(function() {
+        $("#aboutme").fadeIn();
+        $("#portfolio").hide();
+        $("#contactme").hide();
+        $("#firstdiv").hide();
+    })
+
+    $("#head4").click(function() {
+        $("#aboutme").hide();
+        $("#portfolio").fadeIn();
+        $("#contactme").hide();
+        $("#firstdiv").hide();
+    })
+
+    $("#head5").click(function() {
+        $("#aboutme").hide();
+        $("#portfolio").hide();
+        $("#contactme").fadeIn();
+        $("#firstdiv").hide();
+    })
+
+})
